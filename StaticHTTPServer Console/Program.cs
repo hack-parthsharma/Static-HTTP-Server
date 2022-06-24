d4c4a1b38e70942ea9526951e0be2c6ef12490e3
Parth Sharma <69046031+hack-parthsharma@users.noreply.github.com>
2022-06-24 18:27:32 +0530

    Add files via upload

---
+++ StaticHTTPServer Console/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO;
+
+
+namespace StaticHttpSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var path = Directory.GetCurrentDirectory();
+            var port = 80;
+            var httpServer = new StaticHttpServer(path, port);
+            Console.WriteLine("-----[ Static Http Sharp ]-----");
+            Console.WriteLine(string.Format("Starting on http://localhost:{0}", port));
+            Console.WriteLine(string.Format("Watching at {0}", path));
+            try
+            {
+                httpServer.Start();
+            }
+            catch (System.Net.Sockets.SocketException)
+            {
+                Console.WriteLine(string.Format("Port {0} occupied.",port));
+                Console.WriteLine("Exiting...");
+                return;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(string.Format("Unhandled Exception: {0}", ex.Message));
+                Console.WriteLine("Exiting...");
+                return;
+            }
+        }
+    }
+}
